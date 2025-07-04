23d039b5e6ac5aaa7330726b2295f4ed2da6b715
Krishnamaganti7 <krishnamaganti7@gmail.com>
2025-07-04 15:10:40 +0530

    USER

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -27,6 +27,7 @@ function Profile() {
         expertise: user.expertise || "",
       });
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleChange = (e) => {
